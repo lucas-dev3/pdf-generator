102bc16ae32656b706735f3fa414f52f23049b91
Matheu Santos <Matheus.souza@acad.ifma.edu.br>
2023-11-17 00:34:49 -0300

    initial commit

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 const port = 3333;
 const bodyParser = require('body-parser');
 const moment = require('moment');
+var cors = require('cors')
+
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
