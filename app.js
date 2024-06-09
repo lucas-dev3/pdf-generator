034935493676596f7cc934dcacbed008fb1ce332
Matheu Santos <Matheus.souza@acad.ifma.edu.br>
2024-06-09 15:28:14 -0300

    add new billet

--- app.js
+++ app.js
@@ -1,99 +1,82 @@
 const express = require("express");
-const { bill, bradesco } = require("boleto-br");
-const bradescoPDF = require("./dist/index").bradesco;
 const app = express();
 const port = 3333;
-const bodyParser = require('body-parser');
-const moment = require('moment');
-var cors = require('cors')
+const bodyParser = require("body-parser");
+var cors = require("cors");
+const { engine } = require("express-handlebars");
+const mongoose = require("mongoose");
+const puppeteer = require("puppeteer");
+const dotenv = require("dotenv");
+dotenv.config();
 
-app.use(cors())
+mongoose.connect(process.env.MONGODB_URL);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Erro na conexão ao MongoDB:"));
+db.once("open", function () {
+  console.log("Conectado ao MongoDB com sucesso");
+});
+
+app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
-app.post("/", (req, res) => {
-  const {
-    expirationDay,
-    processingDate,
-    documentDate,
-    value,
-    documentNumber,
-    ourNumber,
-    payer
-  } = req.body;
-  const boletos = [
-    {
-      paymentPlace:
-        "Pagável preferencialmente na rede Bradesco ou Bradesco Expresso.",
-      beneficiary: "FEIRAO DOS MOVEIS MAGAZINE LTDA",
-      beneficiaryAddress:
-        "R CEARA 553, NOVA IMPERATRIZ, IMPERATRIZ - MA Cep 65907-90",
-      instructions:
-        "Sr Caixa, não receber após o vencimento.",
-      agency: "2365",
-      agencyDigit: "0",
-      account: 11552,
-      accountDigit: "5",
-      // expirationDay: new Date(2023, 10, 14),
-      expirationDay: new Date(moment(expirationDay, 'YYYY-MM-DD')),
-      documentDate: new Date(moment(documentDate, 'YYYY-MM-DD')),
-      processingDate: new Date(moment(processingDate, 'YYYY-MM-DD')),
-      card: "9",
-      documentNumber: documentNumber,
-      ourNumber: ourNumber,
-      value: value,
-      formatedValue: (value/100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }),
-      documentType: "DM",
-      accept: "N",
-      currencyType: "Real (R$)",
-      amount: "",
-      valueOf: "",
-      descountValue: "",
-      otherDiscounts: "",
-      feeValue: "",
-      outherFees: "",
-      chargeValue: "",
-      payer: payer,
-      // payer: {
-      //   name: "DHYOGOSANTOSLEAL",
-      //   registerNumber: "054.557.173-16",
-      //   street: "AV TESTE TESTE",
-      //   number: "",
-      //   complement: " ",
-      //   district: "CENTRO",
-      //   city: "IMPERATRIZ",
-      //   state: "MA",
-      //   postalCode: "65912-080",
-      // },
-      guarantor: {
-        name: "FEIRAO DOS MOVEIS MAGAZINE LTDA",
-        registerNumber: "074.064.502/0001-12",
-        street: "R CEARA",
-        number: "553",
-        district: "NOVA IMPERATRIZ",
-        complement: " ",
-        city: "Imperatriz",
-        state: "MA",
-        postalCode: "65900-110",
-      },
-    },
-  ];
-  const bradescoBill = bill(bradesco);
-  const { generateBillData } = bradescoBill;
-  generateBillData(boletos)
-    .then((data) => {
-      return bradescoPDF(data[0]); // Supondo que você só quer gerar um boleto
-    })
-    .then((pdf) => {
-      res.contentType("application/pdf");
-      res.send(pdf);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Erro ao gerar boleto");
+app.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const collection = db.collection("transactions");
+    const boleto = await collection.findOne({
+      _id: new mongoose.Types.ObjectId(id),
     });
+
+    if (boleto) {
+      const bill = boleto.bill;
+      console.log("Boleto encontrado:", boleto);
+      return res.render("boleto", {
+        bill,
+        parcela:
+          boleto.installments.length > 1
+            ? boleto.order_id
+            : boleto.installments[0].installment_number,
+        contrato:
+          boleto.installments.length > 1
+            ? "Boleto avulso"
+            : boleto.installments[0].contract_id,
+        oderTitle: boleto.installments.length > 1 ? "Ordem ID" : "Parcela",
+      });
+    } else {
+      return res.status(404).send("Boleto não encontrado.");
+    }
+  } catch (error) {
+    console.error("Erro ao buscar boleto:", error);
+    return res.status(500).send("Erro interno do servidor.");
+  }
+});
+
+app.get("/pdf/:id", async (req, res) => {
+  const { id } = req.params;
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`${process.env.BASE_URL}/${id}`, {
+    waitUntil: "networkidle2",
+  });
+  const pdf = await page.pdf({ format: "A4" });
+  await browser.close();
+
+  res.set({
+    "Content-Type": "application/pdf",
+    "Content-Disposition": `attachment; filename=boleto-${id}.pdf`,
+    "Content-Length": pdf.length,
+  });
+
+  return res.send(pdf);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
